2000cedb508db53739131021dd224ff3ea6dad3a
utsav <utsavkmedzin@gmail.com>
2016-04-25 00:06:50 +0530

    Full app done.

--- app/src/main/java/in/medzin/myapplication/BoxOfficeMoviesAdapter.java
+++ app/src/main/java/in/medzin/myapplication/BoxOfficeMoviesAdapter.java
@@ -1,9 +1,14 @@
 package in.medzin.myapplication;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -19,7 +24,21 @@ public class BoxOfficeMoviesAdapter extends ArrayAdapter<BoxOfficeMovie> {
 
     @Override
     public View getView (int position, View convertView , ViewGroup parent) {
-
-        return null;
+        BoxOfficeMovie movie = getItem(position);
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_box_office_movie, parent, false);
+        }
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvCriticsScore = (TextView) convertView.findViewById(R.id.tvCriticsScore);
+        TextView tvCast = (TextView) convertView.findViewById(R.id.tvCast);
+        ImageView ivPosterImage = (ImageView) convertView.findViewById(R.id.ivPosterImage);
+        // Populate the data into the template view using the data object
+        tvTitle.setText(movie.getTitle());
+        tvCriticsScore.setText("Score: " + movie.getCriticsScore() + "%");
+        tvCast.setText(movie.getCastList());
+        Picasso.with(getContext()).load(movie.getPosterUrl()).into(ivPosterImage);
+        // Return the completed view to render on screen
+        return convertView;
     }
 }
--- app/src/main/java/in/medzin/myapplication/MainActivity.java
+++ app/src/main/java/in/medzin/myapplication/MainActivity.java
@@ -1,13 +1,62 @@
 package in.medzin.myapplication;
 
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import cz.msebera.android.httpclient.Header;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView lvMovies;
+    private BoxOfficeMoviesAdapter adapterMovies;
+    public NetWorkClient client;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvMovies = (ListView) findViewById(R.id.lvMovies);
+        ArrayList<BoxOfficeMovie> aMovies = new ArrayList<BoxOfficeMovie>();
+        adapterMovies = new BoxOfficeMoviesAdapter(this, aMovies);
+        lvMovies.setAdapter(adapterMovies);
+        // Fetch the data remotely
+        fetchBoxOfficeMovies();
+    }
+
+    // Executes an API call to the box office endpoint, parses the results
+    // Converts them into an array of movie objects and adds them to the adapter
+    private void fetchBoxOfficeMovies() {
+        client = new NetWorkClient();
+        client.getBoxOfficeMovies(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject responseBody) {
+                JSONArray items = null;
+                try {
+                    // Get the movies json array
+                    items = responseBody.getJSONArray("movies");
+                    // Parse json array into array of model objects
+                    ArrayList<BoxOfficeMovie> movies = BoxOfficeMovie.fromJson(items);
+                    // Load model objects into the adapter
+                    for (BoxOfficeMovie movie : movies) {
+                        adapterMovies.add(movie); // add movie through the adapter
+                    }
+                    adapterMovies.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
+
